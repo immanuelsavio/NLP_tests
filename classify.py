67c536baabd91e434a9ffe3bcf204e029ff07f5c
immanuelsavio <immanuelsavio@gmail.com>
2019-01-20 14:56:20 +0530

    Create classify.py

---
+++ classify.py
@@ -0,0 +1,4 @@
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.
